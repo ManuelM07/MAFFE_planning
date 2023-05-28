ebc7e4473ced03b51a2ebe34965e05a6173b54f5
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2023-05-27 21:26:44 -0500

    Create app.py & parser for data

---
+++ app/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from planning import Data
+import re
+
+app = Flask(__name__)
+data = ""
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    global data
+
+    uploaded_file = request.files['file']
+    
+    if uploaded_file.filename.endswith('.txt'):
+        file_contents = uploaded_file.read().decode('utf-8')
+
+        return ""
+    else:
+        return render_template('index.html')
+    
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+def parser_data(data):
+    data = re.findall("[\d]+", data)
+    n = int(data[0])
+    min_ = int(data[1])
+    max_ = int(data[2])
+    i=1
+    data_aux = data[3:]
+    values = []
+
+    while True:
+        values.append(data_aux[n*(i-1):n*i])
+        i+=1
+        if i > n:
+            break
+
+    values = [[int(x) for x in row] for row in values]
+    ob_model = Data(n, min_, max_, values)
+    sol = ob_model.get_solution()
+
+    return sol['Cal']
+
+
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
